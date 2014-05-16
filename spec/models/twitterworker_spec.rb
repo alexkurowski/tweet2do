724bce6f8e9d14e23125ed78b952ef94a9d64d74
mapimopi <mapimopipi@gmail.com>
2014-05-16 11:33:42 +0400

    removed arbitrary tests

--- spec/models/twitterworker_spec.rb
+++ spec/models/twitterworker_spec.rb
@@ -5,12 +5,4 @@ describe TwitterWorker do
     return_value = TwitterWorker.start
     expect(return_value).to be_instance_of Twitter::REST::Client
   end
-
-  it "should check new followers" do
-    # expect(TwitterWorker.check_new_followers).not_to eq(:error)
-  end
-
-  it "should check new tasks" do
-    # expect(TwitterWorker.check_new_tasks).not_to eq(:error)
-  end
 end
