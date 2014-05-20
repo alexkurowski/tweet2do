35e2d43135e449426cd5124b9aa6b886b8a9dd6b
mapimopi <mapimopipi@gmail.com>
2014-05-20 10:46:48 +0400

    enhanced readability

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,29 +1,26 @@
 class Task < ActiveRecord::Base
-  def self.add task, user, time_offset
-    text = task.split '@'
-    text[0] = text[0..-2].join if text.count > 2
-    text[1] = "" if text.count < 2
-    time_offset = time_offset.to_i * 60
-
-    is_reminder = false
-    now = Time.now.utc
-    date = parse_time text.last, now, time_offset
-    is_reminder = true if not text.last.empty? and date > now
+  def self.add text, user, offset_in_hours
+    text = text.reverse.split('@', 2).reverse.map(&:reverse)
     
-    Task.create(:text => text.first.strip, :user => user, :is_done => false, :is_reminder => is_reminder, :date => date, :time_offset => time_offset)
+    offset = offset_in_hours.to_i * 60
+
+    date = parse_time(text.last, Time.now.utc, offset) if not text[1].nil?
+    date ||= Time.now.utc
+    
+    is_reminder = text[1] != nil and date > Time.now.utc
+
+    Task.create(:text => text.first.strip, :user => user, :is_done => false, :is_reminder => is_reminder, :date => date, :time_offset => offset)
   end
 
   def self.update id, text
     task = Task.find id
 
-    text = text.split '@'
-    text[0] = text[0..-2].join if text.count > 2
-    text[1] = "" if text.count < 2
+    text = text.reverse.split('@', 2).reverse.map(&:reverse)
+    
+    date = parse_time(text.last, Time.now.utc, task.time_offset) if not text[1].nil?
+    date ||= Time.now.utc
 
-    is_reminder = false
-    now = Time.now.utc
-    date = parse_time text.last, now, task.time_offset
-    is_reminder = true if not text.last.empty? and date > now
+    is_reminder = text[1] != nil and date > Time.now.utc
 
     task.text = text.first.strip
     task.date = date
@@ -38,9 +35,9 @@ class Task < ActiveRecord::Base
   def self.parse_time string, now, time_offset
     date_hash = {}
 
-    string.split(/[^dwm0-9]/).each do |d|
-      date_hash['minute'] = d.to_i if d =~ /^[0-9]+$/ and date_hash.has_key? 'hour'
-      date_hash['hour']   = d.to_i if d =~ /^[0-9]+$/ and not date_hash.has_key? 'hour'
+    string.split(/[^dwmDWM0-9]/).each do |d|
+      date_hash['minute'] = d.to_i         if d =~ /^[0-9]+$/ and date_hash.has_key? 'hour'
+      date_hash['hour']   = d.to_i         if d =~ /^[0-9]+$/ and not date_hash.has_key? 'hour'
       date_hash['day']    = d[0...-1].to_i if d =~ /^[0-9]+[dD]$/
       date_hash['week']   = d[0...-1].to_i if d =~ /^[0-9]+[wW]$/
       date_hash['month']  = d[0...-1].to_i if d =~ /^[0-9]+[mM]$/
@@ -49,11 +46,11 @@ class Task < ActiveRecord::Base
     return now if date_hash.empty?
 
     date_hash['minute'] ||= (date_hash.has_key?('hour') ? 0 : now.min)
-    date_hash['hour'] ||= now.hour
+    date_hash['hour']   ||= now.hour
     date_hash['minute'] = 59 if date_hash['minute'] > 59
-    date_hash['hour'] = 23 if date_hash['hour'] > 23
+    date_hash['hour']   = 23 if date_hash['hour'] > 23
 
-    date = Time.utc now.year, now.month, now.day, date_hash['hour'], date_hash['minute'], now.sec
+    date = Time.utc now.year, now.month, now.day, date_hash['hour'], date_hash['minute']
     date += time_offset
 
     date += date_hash['day'].days     if date_hash.has_key? 'day'
